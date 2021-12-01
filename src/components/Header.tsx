6789065b9a33eeb2ab62e5c09f09707a06250a3b
Micael Rodrigues <micaelr95@outlook.pt>
2021-12-01 01:33:28 +0000

    feat: add web3 react

---
+++ src/components/Header.tsx
@@ -0,0 +1,50 @@
+import { useWeb3React } from '@web3-react/core';
+import { injected } from '../utils/Connectors';
+
+const Header = () => {
+  const { active, account, library, connector, activate, deactivate } =
+    useWeb3React();
+
+  const connect = async () => {
+    try {
+      await activate(injected);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const disconnect = async () => {
+    try {
+      deactivate();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <header className='flex items-center justify-end dark:bg-gray-900 h-16 p-4'>
+      {active ? (
+        <div>
+          <span className='text-gray-900 dark:text-white'>
+            Connected with <b>{account}</b>
+          </span>
+          <button
+            onClick={disconnect}
+            className='px-4 py-2 font-bold text-white bg-blue-600 hover:bg-blue-800'
+          >
+            Disconnect
+          </button>
+        </div>
+      ) : (
+        <button
+          onClick={connect}
+          className='px-4 py-2 font-bold text-white bg-blue-600 hover:bg-blue-800'
+        >
+          Connect to Metamask
+        </button>
+      )}
+    </header>
+  );
+};
+
+export default Header;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,38 @@
 import Head from 'next/head';
+import { useWeb3React } from '@web3-react/core';
+
+import { injected } from '../utils/Connectors';
+import Header from '../components/Header';
 
 export default function Home() {
+  const { active, account, library, connector, activate, deactivate } =
+    useWeb3React();
+
+  const connect = async () => {
+    try {
+      await activate(injected);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const disconnect = async () => {
+    try {
+      deactivate();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
-    <div className='flex flex-col items-center justify-center min-h-screen py-2'>
+    <div>
       <Head>
         <title>Ethereum Todo List Dapp</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <main className='flex flex-col items-center justify-center w-full flex-1 px-20 text-center'></main>
-
-      <footer className='flex items-center justify-center w-full h-24 border-t'></footer>
+      <Header></Header>
+      <main className='flex flex-col items-center justify-center bg-white dark:bg-gray-800 h-screen'></main>
     </div>
   );
 }
